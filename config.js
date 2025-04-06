4bd86905bceb7b9f3ddbf4598009bf9fa2c1a0ec
noriyuki1985 <60012829+noriyuki1985@users.noreply.github.com>
2025-04-07 07:32:12 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // config.js
 const CONFIG = {
-  GOOGLE_MAPS_API_KEY: "AIzaSyBx6jYAfWuQz0E8nIn-B-jy2eZi9y2Pt84"  // ご自身の API キーに置き換えてください
+  GOOGLE_MAPS_API_KEY: "AIzaSyBx6jYAfWuQz0E8nIn-B-jy2eZi9y2Pt84",  // Google Maps API キー
+  ESTAT_APP_ID: "8719992bdbede020708c5e386a9242db05049d3b"  // 取得した e-Stat API の AppID をここに入力
 };
